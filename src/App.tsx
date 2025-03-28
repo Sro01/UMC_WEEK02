0ed883ce7d7af02c918b1b302e977c3cc4c7d9be
Sro01 <sara0099@naver.com>
2025-03-28 11:16:30 +0900

    useState 심화 - 렉시컬 환경(Lexical Environment), prev => prev + 1 연산

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState<number>(0);
+  const handleCount = () => {
+    // setCount(count + 1);
+		setCount((prev) => prev + 1);
+		setCount((prev) => prev + 1);
+		setCount((prev) => prev + 1);
+		setCount((prev) => prev + 1);
+		setCount((prev) => prev + 1);
+  };
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={handleCount}>+1</button>
     </>
   );
 }
